2e31203d2028bffe8ab175af701e6f5095895012
shvedpavel <pav159208@gmail.com>
2023-12-19 00:07:54 +0300

    created new projeck

---
+++ SliderSwiftUI/SliderSwiftUI/SliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSwiftUIApp.swift
+//  SliderSwiftUI
+//
+//  Created by Apple on 14.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
